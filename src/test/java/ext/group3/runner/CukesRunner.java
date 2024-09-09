6578b83103862f05a0ecbf52a1b8e2ed7d195ed9
Alex Cheble <45234022+Alexcheble@users.noreply.github.com>
2024-09-09 17:43:37 -0400

    first commit

--- src/test/java/ext/group3/runner/CukesRunner.java
+++ src/test/java/ext/group3/runner/CukesRunner.java
@@ -36,8 +36,8 @@ import org.junit.runner.RunWith;
                 "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" },
         features = "src/test/resources/features", //goes to this folder where file is stored
         glue = "io/loop/step_definitions", //helps connect my feature file and step defs java code
-        dryRun = false,  //gets code snippet for this scenario with the below tag when true
-        tags = "@B3G3-169",
+        dryRun = true,  //gets code snippet for this scenario with the below tag when true
+        tags = "@docuportLoginForALlUsers",
         monochrome = false,//reduces unnecessary locks
         publish = false //creates online cucumber reports that can be opened in the browser
 
