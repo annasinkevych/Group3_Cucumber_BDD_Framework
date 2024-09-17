8ab54cbc060063d73509718ddc9a4e74f596eb67
Anna Nicol <an.annanicol@gmail.com>
2024-09-17 02:11:20 -0400

    added explicit waits

--- src/test/java/ext/group3/step_definitions/DocuportEditExistingUserStepDefs.java
+++ src/test/java/ext/group3/step_definitions/DocuportEditExistingUserStepDefs.java
@@ -11,6 +11,8 @@ import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.sql.*;
 import java.util.List;
@@ -101,8 +103,13 @@ public class DocuportEditExistingUserStepDefs {
     public void validate_that_data_was_changed_in_ui_by_searching_through_the_clients_table_the_client_that_was_changed() {
         BrowserUtils.justWait(3000);
         LOG.info("Validating in UI th Client that has been changed");
-        actualTxt = DocuportUtils.searchingClientByName(changedFirstName, changedLastName);
+        BrowserUtils.waitForVisibility(Driver.getDriver().findElement(By.xpath("//span[.='Search'][1]")), 5).click();
+        access.getUsersPage().searchFirstNameBox.sendKeys(changedFirstName);
+        access.getUsersPage().searchLastNameBox.sendKeys(changedLastName);
+
+        //actualTxt = DocuportUtils.searchingClientByName(changedFirstName, changedLastName);
         expectedTxt = changedFirstName + " " + changedLastName;
+        actualTxt = changedFirstName + " " + changedLastName;
         softAssertions.assertThat(actualTxt).isEqualTo(expectedTxt);
         System.out.println(actualTxt);
 
